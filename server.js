d919cd840fb51f47c90dae91c68ba9e420ddb47e
Kavya <kavyakopatti99@gmail.com>
2025-02-25 08:26:09 +0530

    cors

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const mongoose=require('mongoose')
 let port = process.env.PORT || 8000;
 
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(cors({  origin: ["http://localhost:3000", "https://vege-foods.shop"], credentials: true }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
