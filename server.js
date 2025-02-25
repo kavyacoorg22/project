863b5b53c9b91305bd65d60763ba53719d9cc259
Kavya <kavyakopatti99@gmail.com>
2025-02-25 06:52:44 +0530

    old code

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.use((req, res, next) => {
 app.use('/user',userRouter)
 app.use('/admin',adminRouter)
 app.get('/', (req, res) => {
-  res.redirect('/user/home');
+  res.redirect(`/user/home`);
 });
 
 
